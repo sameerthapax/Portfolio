5efd28a1ea89b0c0560fe313fd7eec6aafcaa662
Sameer Thapa <101423564+sameerthapax@users.noreply.github.com>
2024-07-17 08:59:48 -0500

    designing

--- script.js
+++ script.js
@@ -310,53 +310,76 @@ projectCard.to('#projects-container',{xPercent:100,opacity:100,ease:'power4.inOu
 //     },
 //     ease: "power4.inOut",
 // });
+//
+// const tl = new gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '#page-title',
+//         start: 'top top',
+//         end: 'bottom top',
+//         scrub: true,
+//         markers: false,
+//     }
+// });
+//
+// const tl3 = new gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '#page-title',
+//         start: 'top top',
+//         end: '60% top',
+//         scrub: true,
+//         markers: false,
+//     }
+// });
+//
+// const tl2 = new gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '#page-title',
+//         start: '175% top',
+//         endTrigger: '#page3',
+//         end: 'top top',
+//         scrub: true,
+//         markers: false,
+//     }
+// });
+//
+// const tl4 = new gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '#page-title',
+//         start: '175% top',
+//         endTrigger: '#page3',
+//         end: 'top top',
+//         scrub: true,
+//         markers: false,
+//     }
+// });
+//
+// tl.from("#page-title", {y: -700, duration: 2});
+// tl3.from("#title-background", {y: 700, duration: 2});
+// tl2.to("#page-title", {y: 700, duration: 2});
+// tl4.to("#title-background", {y: -700, duration: 2});
 
-const tl = new gsap.timeline({
-    scrollTrigger: {
-        trigger: '#page-title',
-        start: 'top top',
-        end: 'bottom top',
-        scrub: true,
-        markers: false,
-    }
-});
 
-const tl3 = new gsap.timeline({
-    scrollTrigger: {
+const page2TitleIntro = new gsap.timeline({scrollTrigger:{
         trigger: '#page-title',
-        start: 'top top',
-        end: '60% top',
-        scrub: true,
-        markers: false,
-    }
-});
-
-const tl2 = new gsap.timeline({
-    scrollTrigger: {
-        trigger: '#page-title',
-        start: '175% top',
+        start: '50% top',
         endTrigger: '#page3',
         end: 'top top',
         scrub: true,
-        markers: false,
-    }
-});
+        markers: true,
+    }});
+page2TitleIntro.from("#page-title",{y:-700, duration:2,ease:'power4.inOut'})
+page2TitleIntro.to("#page-title", {y: 700, duration: 2,delay:2, ease: 'power4.inOut'});
 
-const tl4 = new gsap.timeline({
-    scrollTrigger: {
+const page2TitleBackgroundIntro = new gsap.timeline({scrollTrigger:{
         trigger: '#page-title',
-        start: '175% top',
+        start: '50% top',
         endTrigger: '#page3',
         end: 'top top',
         scrub: true,
-        markers: false,
-    }
-});
-
-tl.from("#page-title", {y: -700, duration: 2});
-tl3.from("#title-background", {y: 700, duration: 2});
-tl2.to("#page-title", {y: 700, duration: 2});
-tl4.to("#title-background", {y: -700, duration: 2});
+        markers: true,
+    }});
+page2TitleBackgroundIntro.from("#title-background",{y:700, duration:2,ease:'power4.inOut'})
+page2TitleBackgroundIntro.to("#title-background", {y: -700, duration: 2,delay:2, ease: 'power4.inOut'});
 
 window.addEventListener("load", function () {
     lenis.stop();
@@ -401,126 +424,6 @@ function removeLoader() {
     });
 
 }
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
-
-let w, h, particles;
-let particleDistance = 20;
-let mouse = {
-    x: undefined,
-    y: undefined,
-    radius: 50
-}
-
-function init() {
-    resizeReset();
-    animationLoop();
-}
-
-function resizeReset() {
-    w = canvas.width = window.innerWidth;
-        h = canvas.height = window.innerHeight;
-
-    particles = [];
-    for (let y = (((h - particleDistance) % particleDistance) + particleDistance) / 2; y < h; y += particleDistance) {
-        for (let x = (((w - particleDistance) % particleDistance) + particleDistance) / 2; x < w; x += particleDistance) {
-            particles.push(new Particle(x, y));
-        }
-    }
-}
-
-function animationLoop() {
-    ctx.clearRect(0, 0, w, h);
-    drawScene();
-    requestAnimationFrame(animationLoop);
-}
-
-function drawScene() {
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
-        particles[i].draw();
-    }
-    drawLine();
-}
-
-function drawLine() {
-    for (let a = 0; a < particles.length; a++) {
-        for (let b = a; b < particles.length; b++) {
-            let dx = particles[a].x - particles[b].x;
-            let dy = particles[a].y - particles[b].y;
-            let distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < particleDistance * 1.5) {
-                let opacity = 1 - (distance / (particleDistance * 1.5));
-                ctx.strokeStyle = "rgba(0,0,0," + opacity + ")";
-                ctx.lineWidth = 2;
-                ctx.beginPath();
-                ctx.moveTo(particles[a].x, particles[a].y);
-                ctx.lineTo(particles[b].x, particles[b].y);
-                ctx.stroke();
-            }
-        }
-    }
-}
-
-function mousemove(e) {
-    mouse.x = e.x;
-    mouse.y = e.y;
-}
-
-function mouseout() {
-    mouse.x = undefined;
-    mouse.y = undefined;
-}
-
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = 4;
-        this.baseX = this.x;
-        this.baseY = this.y;
-        this.speed = (Math.random() * 25) + 5;
-    }
-    draw() {
-        ctx.fillStyle = "rgba(255,255,255,1)";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
-    }
-    update() {
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let distance = Math.sqrt(dx * dx + dy * dy);
-        let maxDistance = mouse.radius;
-        let force = (maxDistance - distance) / maxDistance; // 0 ~ 1
-        let forceDirectionX = dx / distance;
-        let forceDirectionY = dy / distance;
-        let directionX = forceDirectionX * force * this.speed;
-        let directionY = forceDirectionY * force * this.speed;
-
-        if (distance < mouse.radius) {
-            this.x -= directionX;
-            this.y -= directionY;
-        } else {
-            if (this.x !== this.baseX) {
-                let dx = this.x - this.baseX;
-                this.x -= dx / 10;
-            }
-            if (this.y !== this.baseY) {
-                let dy = this.y - this.baseY;
-                this.y -= dy / 10;
-            }
-        }
-    }
-}
-
-init();
-window.addEventListener("resize", resizeReset);
-window.addEventListener("mousemove", mousemove);
-window.addEventListener("mouseout", mouseout);
-
 
 document.querySelectorAll('nav a').forEach(link => {
     link.addEventListener('click', function (e) {
