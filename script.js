d9a4ef1930e681046f3cd82f9a66e54d3675c9fb
Sameer Thapa <101423564+sameerthapax@users.noreply.github.com>
2024-08-04 08:59:58 -0500

    designing

--- script.js
+++ script.js
@@ -169,6 +169,85 @@ gsap.to("#ContactNevButton", {
     },
     backgroundColor: "rgba(248,4,4,0.56)",
 });
+class HoverButton {
+    constructor(el) {
+        this.el = el;
+        this.hover = false;
+        this.calculatePosition();
+        this.attachEventsListener();
+    }
+
+    attachEventsListener() {
+        window.addEventListener('mousemove', e => this.onMouseMove(e));
+        window.addEventListener('resize', e => this.calculatePosition(e));
+    }
+
+    calculatePosition() {
+        gsap.set(this.el, {
+            x: 0,
+            y: 0,
+            scale: 1
+        });
+        const box = this.el.getBoundingClientRect();
+        this.x = box.left + (box.width * 0.5);
+        this.y = box.top + (box.height * 0.5);
+        this.width = box.width;
+        this.height = box.height;
+    }
+
+    onMouseMove(e) {
+        let hover = false;
+        let hoverArea = (this.hover ? 0.4 : 0.3);
+        let x = e.clientX - this.x;
+        let y = e.clientY - this.y;
+        let distance = Math.sqrt( x*x + y*y );
+        if (distance < (this.width * hoverArea)) {
+            hover = true;
+            if (!this.hover) {
+                this.hover = true;
+            }
+            this.onHover(e.clientX, e.clientY);
+        }
+
+        if(!hover && this.hover) {
+            this.onLeave();
+            this.hover = false;
+        }
+    }
+
+    onHover(x, y) {
+        gsap.to(this.el,  {
+            x: (x - this.x) * 0.4,
+            y: (y - this.y) * 0.4,
+            scale: 1,
+            ease: 'power2.out',
+            duration: 0.4
+        });
+        this.el.style.zIndex = 100;
+    }
+    onLeave() {
+        gsap.to(this.el, {
+            x: 0,
+            y: 0,
+            scale: 1,
+            ease: 'elastic.out(1.2, 0.4)',
+            duration: 0.7
+        });
+        this.el.style.zIndex = 1;
+    }
+}
+
+const btn1 = document.querySelector('#homeNevButton');
+new HoverButton(btn1);
+
+const btn2 = document.querySelector('#aboutNevButton');
+new HoverButton(btn2);
+
+const btn3 = document.querySelector('#projectNevButton');
+new HoverButton(btn3);
+const btn4 = document.querySelector('#ContactNevButton');
+new HoverButton(btn4);
+
 //navigation-end
 
 // gsap.to("#page2", {
@@ -437,10 +516,10 @@ webkitBackdropFilter: "blur(2vh)",width:"99vw",border: "solid #e9e9e9 1px", ease
 
 $(function() {
     $('.navButton').hover(function() {
-        $('.circle').css('opacity','0');
+        $('.circle').css('opacity','0.2');
     }, function() {
         // on mouseout, reset the background colour
-        $('.circle').css('opacity', '100');
+        $('.circle').css('opacity', '1');
     });
 })
 const date = new Date();
